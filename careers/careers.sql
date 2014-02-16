2c5b1321039c867ceb029cdf1698896e955bbbf5
CapstoneMU <mcwrmd@mail.missouri.edu>
2014-02-16 14:53:36 -0600

    Added to SQL file and degrees.php
    added authentication to sql and other tables.  Added some degree
    programs to choose from on degrees.php.  removed some sql errors.

--- careers/careers.sql
+++ careers/careers.sql
@@ -1,53 +1,105 @@
 -- careers.sql for Grad Career Application
--- creates tables in database for careers schema
+-- creates tables in database for careerSchema
 
--- matt weiner
+-- team x
 
 DROP SCHEMA IF EXISTS careerSchema;
-
 CREATE SCHEMA careerSchema;
+
 
 SET search_path = careerSchema, public;
 
-DROP TABLE IF EXISTS graduates;
-CREATE TABLE graduates (
+
+DROP TABLE IF EXISTS careerSchema.graduates;
+CREATE TABLE careerSchema.graduates (
+	username	varchar(50) PRIMARY KEY,
 	firstName 	varchar(50),
 	lastName	varchar(50),
 	degree		varchar(50),
-	degreeCode	int
+	degreeCode	int,
+	linkedIn	varchar(50)
 );
 
-INSERT INTO graduates VALUES ('Matthew','Weiner','IT',1);
 
-DROP TABLE IF EXISTS companies;
-CREATE TABLE companies (
-	companyID int,
-	name varchar(50),
-	address varchar(50),
-	city varchar(50),
-	zipcode int,
+INSERT INTO careerSchema.graduates VALUES ('Matthew','Weiner','IT',1);
+
+
+-- Table: careerSchema.graduateAuthentication
+-- Columns:
+--    username      - The username tied to the authentication info.
+--    password_hash - The hash of the user's password + salt. Expected to be SHA1.
+--    salt          - The salt to use. Expected to be a SHA1 hash of a random input.
+DROP TABLE IF EXISTS careerSchema.graduateAuthentication
+CREATE TABLE careerSchema.graduateAuthentication (
+	username 		VARCHAR(30) PRIMARY KEY,
+	password_hash 	CHAR(40) NOT NULL,
+	salt 			CHAR(40) NOT NULL,
+	FOREIGN KEY (username) REFERENCES careerSchema.graduates(username)
+);
+
+
+DROP TABLE IF EXISTS careerSchema.companies;
+CREATE TABLE careerSchema.companies (
+	companyID 	int,
+	name 		varchar(50),
+	address 	varchar(50),
+	city 		varchar(50),
+	zipcode 	int,
 	PRIMARY KEY (companyID)
 );
 
-INSERT INTO companies VALUES (1,'IBM','2810 Lemone Industrial','Columbia', 65201);
---DROP TABLE IF EXISTS company_jobs;
---CREATE TABLE company_jobs (
+
+INSERT INTO careerSchema.companies VALUES (1,'IBM','2810 Lemone Industrial','Columbia', 65201);
+
+
+--DROP TABLE IF EXISTS careerSchema.company_jobs;
+--CREATE TABLE careerSchema.company_jobs (
 	
 --);
 
-DROP TABLE IF EXISTS jobs;
-CREATE TABLE jobs (
+DROP TABLE IF EXISTS careerSchema.jobs;
+CREATE TABLE careerSchema.jobs (
 	jobID		int,
 	jobTitle	varchar(50),
 	compnay		varchar(50),
 	PRIMARY KEY (jobID)
 );
 
-DROP TABLE IF EXISTS admins;
-CREATE TABLE admins (
-	adminID		int,
-	username	varchar(50),
-	firstName	varchar(50),
-	lastName	varchar(50),
-	PRIMARY KEY (adminID)
+DROP TABLE IF EXISTS careerSchema.admin_info;
+CREATE TABLE careerSchema.admin_info (
+	username			varchar(50) PRIMARY KEY,
+	firstName			varchar(50),
+	lastName			varchar(50),
+	registration_date 	TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	description			varchar(50),
+	PRIMARY KEY (username)
 );
+
+-- Table: careerSchema.adminAuthentication
+-- Columns:
+--    username      - The username tied to the authentication info.
+--    password_hash - The hash of the user's password + salt. Expected to be SHA1.
+--    salt          - The salt to use. Expected to be a SHA1 hash of a random input.
+CREATE TABLE careerSchema.adminAuthentication (
+	username 		VARCHAR(30) PRIMARY KEY,
+	password_hash 	CHAR(40) NOT NULL,
+	salt 			CHAR(40) NOT NULL,
+	FOREIGN KEY (username) REFERENCES careerSchema.admin_info(username)
+);
+
+-- Table: careerSchema.log
+-- Columns:
+--    log_id     - A unique ID for the log entry. Set by a sequence.
+--    username   - The user whose action generated this log entry.
+--    ip_address - The IP address of the user at the time the log was entered.
+--    log_date   - The date of the log entry. Set automatically by a default value.
+--    action     - What the user did to generate a log entry (i.e., "logged in").
+CREATE TABLE careerSchema.log (
+	log_id  	SERIAL PRIMARY KEY,
+	username 	VARCHAR(30) NOT NULL REFERENCES lab8.user_info,
+	ip_address 	VARCHAR(15) NOT NULL,
+	log_date 	TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	action 		VARCHAR(50) NOT NULL
+);
+
+CREATE INDEX log_log_id_index ON careerSchema.log (username);
---
+++ careers/index.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>CapstoneCareers</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+	<!--link rel="stylesheet/less" href="less/bootstrap.less" type="text/css" /-->
+	<!--link rel="stylesheet/less" href="less/responsive.less" type="text/css" /-->
+	<!--script src="js/less-1.3.3.min.js"></script-->
+	<!--append ‘#!watch’ to the browser URL, then refresh the page. -->
+	
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+
+  <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+  <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+  <![endif]-->
+
+  <!-- Fav and touch icons -->
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/apple-touch-icon-144-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/apple-touch-icon-114-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/apple-touch-icon-72-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" href="img/apple-touch-icon-57-precomposed.png">
+  <link rel="shortcut icon" href="img/favicon.png">
+  
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/scripts.js"></script>
+</head>
+
+<body>
+<div class="container">
+	<div class="row clearfix">
+		<div class="col-md-12 column">
+			<nav class="navbar navbar-default" role="navigation">
+				<div class="navbar-header">
+					 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button> <a class="navbar-brand" href="#">Brand</a>
+				</div>
+				
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li class="active">
+							<a href="#">Link</a>
+						</li>
+						<li>
+							<a href="#">Link</a>
+						</li>
+						<li class="dropdown">
+							 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown<strong class="caret"></strong></a>
+							<ul class="dropdown-menu">
+								<li>
+									<a href="#">Action</a>
+								</li>
+								<li>
+									<a href="#">Another action</a>
+								</li>
+								<li>
+									<a href="#">Something else here</a>
+								</li>
+								<li class="divider">
+								</li>
+								<li>
+									<a href="#">Separated link</a>
+								</li>
+								<li class="divider">
+								</li>
+								<li>
+									<a href="#">One more separated link</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+					<form class="navbar-form navbar-left" role="search">
+						<div class="form-group">
+							<input type="text" class="form-control">
+						</div> <button type="submit" class="btn btn-default">Submit</button>
+					</form>
+					<ul class="nav navbar-nav navbar-right">
+						<li>
+							<a href="#">Link</a>
+						</li>
+						<li class="dropdown">
+							 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown<strong class="caret"></strong></a>
+							<ul class="dropdown-menu">
+								<li>
+									<a href="#">Action</a>
+								</li>
+								<li>
+									<a href="#">Another action</a>
+								</li>
+								<li>
+									<a href="#">Something else here</a>
+								</li>
+								<li class="divider">
+								</li>
+								<li>
+									<a href="#">Separated link</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+				
+			</nav>
+			<div class="jumbotron">
+				<h1>
+					Careers for Grads
+				</h1>
+				<p>
+					This application allows Students who are near graduation to find Real entry level careers in their field
+				</p>
+				<p>
+					<a class="btn btn-primary btn-large" href=http://babbage.cs.missouri.edu/~mcwrmd/careers/degrees.php>Find your Career</a>
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="row clearfix">
+		<div class="col-md-4 column">
+			<h2>
+				Why Every Student Needs a Career Before Graduation
+			</h2>
+			<p>
+				We do not host positions for senior level positions or part time non professional positions.  Put fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+			</p>
+			<p>
+				<a class="btn" href="#">View details »</a>
+			</p>
+		</div>
+		<div class="col-md-4 column">
+			<h2>
+				Company Hub
+			</h2>
+			<p>
+				Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+			</p>
+			<p>
+				<a class="btn" href="#">View details »</a>
+			</p>
+		</div>
+		<div class="col-md-4 column">
+			<h2>
+				Admin Hub
+			</h2>
+			<p>
+				Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+			</p>
+			<p>
+				<a class="btn" href="#">View details »</a>
+			</p>
+		</div>
+	</div>
+</div>
+</body>
+</html>
