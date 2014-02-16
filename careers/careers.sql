c46e9902d58fda5569a6a4619285c5cdc324b66d
CapstoneMU <mcwrmd@mail.missouri.edu>
2014-02-15 19:57:36 -0600

    Better Sql
    added 2 demo inserts and made all tables create

--- careers/careers.sql
+++ careers/careers.sql
@@ -3,38 +3,37 @@
 
 -- matt weiner
 
--- drop tables
-DROP TABLE IF EXISTS careerSchema.graduates;
-DROP TABLE IF EXISTS careerSchema.companies;
-DROP TABLE IF EXISTS careerSchema.company_jobs;
-DROP TABLE IF EXISTS careerSchema.jobs;
-DROP TABLE IF EXISTS careerSchema.admins;
+DROP SCHEMA IF EXISTS careerSchema;
 
 CREATE SCHEMA careerSchema;
 
-SET search_path = careers, public;
+SET search_path = careerSchema, public;
 
 DROP TABLE IF EXISTS graduates;
 CREATE TABLE graduates (
 	firstName 	varchar(50),
 	lastName	varchar(50),
-	degree		varchar(50)
+	degree		varchar(50),
+	degreeCode	int
 );
+
+INSERT INTO graduates VALUES ('Matthew','Weiner','IT',1);
 
 DROP TABLE IF EXISTS companies;
 CREATE TABLE companies (
-	compnayID int,
+	companyID int,
 	name varchar(50),
 	address varchar(50),
 	city varchar(50),
-	zipcode int
+	zipcode int,
 	PRIMARY KEY (companyID)
 );
 
-DROP TABLE IF EXISTS company_jobs;
-CREATE TABLE company_jobs (
+INSERT INTO companies VALUES (1,'IBM','2810 Lemone Industrial','Columbia', 65201);
+--DROP TABLE IF EXISTS company_jobs;
+--CREATE TABLE company_jobs (
 	
-);
+--);
 
 DROP TABLE IF EXISTS jobs;
 CREATE TABLE jobs (
